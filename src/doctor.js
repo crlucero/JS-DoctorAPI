5d63e0c8bac42f7020010839df90a99d7faa99af
Cristian Lucero <cristianjlucero32@gmail.com>
2019-01-20 11:05:57 -0800

    This commit has full functionality. Previous info still needs to be cleared when new search happens

--- src/doctor.js
+++ src/doctor.js
@@ -1,22 +1,22 @@
 export class Doctor {
-  constructor(firstName, lastName, address, phone, patientStatus) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.address = address;
-    this.phone = phone;
-    this.patientStatus = patientStatus;
-  }
+    constructor(firstName, lastName, address, phone, patientStatus) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.patientStatus = patientStatus;
+    }
 }
 
 export class Specialty {
 
     // This is the main API query that will grab doctors and a specialty
     getDoctorBySpecialty(name, specialty) {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             let request = new XMLHttpRequest();
             let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.exports.apiKey}&location=wa-seattle&query=${specialty}&name=${name}`;
-            request.onload = function() {
-                if(this.status === 200) {
+            request.onload = function () {
+                if (this.status === 200) {
                     resolve(request.response);
                 } else {
                     reject(Error(request.statusText));
@@ -27,3 +27,32 @@ export class Specialty {
         });
     }
 } 
+
+// export class Doctor {
+//   constructor(firstName, lastName, address, phone, patientStatus) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.address = address;
+//     this.phone = phone;
+//     this.patientStatus = patientStatus;
+//   }
+// }
+
+// export class Specialty {
+//     // This is the main API query that will grab doctors and a specialty
+//     getDoctorBySpecialty(name, specialty) {
+//         return new Promise(function(resolve, reject) {
+//             let request = new XMLHttpRequest();
+//             let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+//             request.onload = function() {
+//                 if(this.status === 200) {
+//                     resolve(request.response);
+//                 } else {
+//                     reject(Error(request.statusText));
+//                 }
+//             }
+//             request.open("GET", url, true);
+//             request.send();
+//         });
+//     }
+// } 
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   $("form#drSearch").submit(function() {
     event.preventDefault();
     let name = $("input#drName").val();
-    let specialty = $("input#specialty").val
+    let specialty = $("input#specialty").val();
 
     let searchSpecialty = new Specialty();
     let promise = searchSpecialty.getDoctorBySpecialty(name, specialty);
@@ -27,21 +27,19 @@ $(document).ready(function() {
         let lastName = body.data[x].profile.last_name;
         let address = `${body.data[x].practices[0].visit_address.street}
           ${body.data[x].practices[0].visit_address.city}, ${body.data[x].practices[0].visit_address.state} ${body.data[x].practices[0].visit_address.zip}`;
-        let phone = body.data[x].practices[0].phones.number;
-        let patients = body.data[x].practices[0].accepts_new_patients;
+        let phone = body.data[x].practices[0].phones[0].number;
+        let patientStatus = body.data[x].practices[0].accepts_new_patients;
 
-        let doctor = new Doctor(firstName, lastName, address, phone, patients);
+        let doctor = new Doctor(firstName, lastName, address, phone, patientStatus);
 
-        $('#results-table').append(
-          `<tr>
+        $("#results-table").append(`<tr>
             <td>${doctor.firstName}</td>
             <td>${doctor.lastName}</td>
             <td>${doctor.address}</td>
             <td>${doctor.phone}</td>
-            <td>${doctor.newPatients}</td>
-          </tr>`
-        );
-      }
+            <td>${doctor.patientStatus}</td>
+          </tr>`);
+      } 
     }, function (error) {
       $('#errorMessage').text(`We could not process your request. Please try again. ${error}`);
     });
